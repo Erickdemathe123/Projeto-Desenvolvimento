18a719eb0de5c41bd904a13b5daf22404d94b385
YagoAyala <yago.ayala@outlook.com>
2025-05-16 15:07:08 -0300

    feat: created initial files

---
+++ client/src/app/page.js
@@ -0,0 +1,15 @@
+import SignIn from "../components/otherPages/SignIn";
+export const metadata = {
+  title:
+    "Home 1 || Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+  description:
+    "Lexend - Full-featured, professional-looking software, saas and startup nextjs template.",
+};
+
+export default function HomePage1() {
+  return (
+    <>
+      <SignIn/>
+    </>
+  );
+}
